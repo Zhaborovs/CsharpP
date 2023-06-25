d81b990e87662cdee96676024462c82b9b1f8fe7
Danila_Zh <denis_petrovich_02@bk.ru>
2023-06-25 05:44:50 +0500

    added Task004_new

---
+++ Task004/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 4: Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел */
+
+Console.WriteLine("введите целочисленное число 1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите целочисленное число 2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите целочисленное число 3 ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+int cout = 0;
+while (cout=2)
+{
+if
+cout++;
+}
