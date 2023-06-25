b041218067e261869094869cd574a7d74cc93420
Danila_Zh <denis_petrovich_02@bk.ru>
2023-06-25 23:20:37 +0500

    homeWork done

---
+++ Task008/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Задача 8: Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N. */
+
+ Console.WriteLine("введите целочисленное число N ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int cout = 0;
+while(cout <= number1){
+if(0 == cout % 2){Console.WriteLine(cout);}
+cout +=2;
+}
