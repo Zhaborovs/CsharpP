6f5bfcd6711494cd5c62deadd7d6b047c1d00cd0
Danila_Zh <denis_petrovich_02@bk.ru>
2023-06-25 23:13:59 +0500

    added Task008

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 6: Напишите программу, которая на вход
+принимает число и выдаёт, является ли число чётным
+(делится ли оно на два без остатка).
+ */
+
+ Console.WriteLine("введите целочисленное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if(0 == number1 % 2){Console.WriteLine($" {number1} является четным числом. ");}
+else {Console.WriteLine($" {number1} не является целочислиным или четным числом. ");}
