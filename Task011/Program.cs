9fb4205e1d9ed1d0dbae17702122a815aec35ff8
Danila_Zh <denis_petrovich_02@bk.ru>
2023-06-27 00:25:13 +0500

    added Task13

--- Task011/Program.cs
+++ Task011/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 11. Напишите программу, которая выводит случайное
+трёхзначное число и удаляет вторую цифру этого
+числа.
+456 -> 46
+782 -> 72
+918 -> 98 */
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число из отрезка 100 - 999 -> {number}");
+
+int firstDigit = (number / 100)*10; //умнажением делаем цифрой "десятков"
+int secondDigit = number % 10;
+int lastDigit = firstDigit + secondDigit;
+Console.WriteLine($"число без цифры посередине -> {lastDigit}");
